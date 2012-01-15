e3e11295a7a87374fd317febeba7cf679b6bf6fe
rverbee <russellverbeeten@gmail.com>
2012-01-15 21:35:53 +0000

    this is a commit to a reposity I created to test git. Still learning...

---
+++ app/controllers/say_controller.rb
@@ -0,0 +1,12 @@
+class SayController < ApplicationController
+  def hello
+  	@time = Time.now;
+	@files = Dir.glob('*');
+  end
+
+  def goodbye
+  end
+  
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '616fb411549f93f97bb59cd27df7db4266bc4c034c7bc61fc6aa2f359ab8b22908288048d866146bfd6b697f37df09eb7d5cc54928b3be0b64fd652372b0c9f5'
